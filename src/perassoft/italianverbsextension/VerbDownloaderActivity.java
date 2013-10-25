ca0f4820f095dcd372fa94dc25c9a52ffa77e76c
Marco <marco.perasso@gmail.com>
2013-10-25 13:55:18 +0200

    traduzioni

--- src/perassoft/italianverbsextension/VerbDownloaderActivity.java
+++ src/perassoft/italianverbsextension/VerbDownloaderActivity.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.net.URLEncoder;
 
 import android.app.Activity;
 import android.content.Intent;
